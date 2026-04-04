d22dddf29fc6b0aed0adbaa4f98576d328762d8a
Your Name <you@example.com>
2026-04-04 13:42:21 +0500

    add repo name in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD THIS LINE
+  base: mode === 'production' ? '/isha-waris.github.io/' : '/', 
   server: {
     host: "::",
     port: 5173,
